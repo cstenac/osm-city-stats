e3f9f1cafb67c22c81d99c2eb28f4e3f3d85e2fb
Clément Stenac <clement.stenac@gmail.com>
2012-07-25 12:15:38 +0200

    Disable the tags_count columns for the moment, not yet used.

--- backend/s1-prepare-analysis.sql
+++ backend/s1-prepare-analysis.sql
@@ -3,8 +3,12 @@
 --- Step 1 of the analysis: some indexes
 
 -- Fast access for nodes, ways with no tags
-ALTER TABLE nodes add tags_count INTEGER; CREATE INDEX nodes_tags_count ON nodes(tags_count);
-ALTER TABLE ways add tags_count INTEGER; CREATE INDEX ways_tags_count ON ways(tags_count);
 
-UPDATE nodes SET tags_count = array_length(avals(tags), 1);
-UPDATE ways SET tags_count = array_length(avals(tags), 1);
+-- Not used at the moment, disable it.
+--ALTER TABLE nodes add tags_count INTEGER;
+--CREATE INDEX nodes_tags_count ON nodes(tags_count);
+--ALTER TABLE ways add tags_count INTEGER;
+--CREATE INDEX ways_tags_count ON ways(tags_count);
+--
+--UPDATE nodes SET tags_count = array_length(avals(tags), 1);
+--UPDATE ways SET tags_count = array_length(avals(tags), 1);
