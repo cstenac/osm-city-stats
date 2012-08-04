d5aa5af994aaf4f23418510253dc4c4de95700b7
Clément Stenac <clement.stenac@gmail.com>
2012-08-04 11:50:02 +0200

    Compute data in several steps to have better index locality

--- backend/s5-compute-city-data.php
+++ backend/s5-compute-city-data.php
@@ -1,7 +1,8 @@
 <?php
   /**
-   * Step 5 of the analysis: compute the ways and nodes per city, and compute
-   *  the aggregated city data
+   * Step 5 of the analysis: compute the ways and nodes per city.
+   * We iterate 3 times on the cities, to have better index locality
+   * on the nodes, way_geometry and closedway_geometry indexes
    */
 
   $modulo = 1;
@@ -44,41 +45,84 @@
 
   $conn = pg_connect($db_conn_string) or die('Could not connect: ' . pg_last_error());
 
+  $count = get_one_data("SELECT count(id) as count from  relations WHERE tags -> 'admin_level' = '8'", "count");
+
 
   $query = "SELECT id, tags -> 'name' as name from relations WHERE tags -> 'admin_level' = '8'";
-  $result = pg_query($query);
 
+
+
+  /* First loop: city_way */
+  pg_query("BEGIN");
+  $result = pg_query($query);
   $loop_index = 0;
   while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     $id = $line["id"];
     $name = $line["name"];
-
-    
     if ($loop_index++ % $modulo != $selected_modulo) {
         echo "Ignoring $name ($id) (modulo)\n";
         continue;
     }
-  
     # Start by cleaning our own data to make this script replayable
-    echo "Working for $name ($id)\n";
     pg_query("DELETE FROM city_way where relation_id=$id");
-    pg_query("DELETE FROM city_node where relation_id=$id");
+    echo "Computing city ways for $name ($id) (progress: ".($loop_index-1)."/$count)\n";
+    time_start();
+  #  pg_query("INSERT INTO city_way (relation_id, way_id, geog) SELECT $id,w.id,  wg.geom from ways w, city_geom cg, way_geometry wg WHERE wg.way_id = w.id AND ST_Intersects(cg.geom_dump, wg.geom) AND cg.relation_id=$id");
+    pg_query("INSERT INTO city_way SELECT $id,w.id, w.tags,  wg.geom from ways w, city_geom cg, way_geometry wg WHERE wg.way_id = w.id AND ST_Intersects(cg.geom_dump, wg.geom) AND cg.relation_id=$id");
+    time_end("ways");
+  }
+  pg_query("COMMIT");
+
+  /* Second loop: city_closedway */
+  $result = pg_query($query);
+  $loop_index = 0;
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    $id = $line["id"];
+    $name = $line["name"];
+    if ($loop_index++ % $modulo != $selected_modulo) {
+        echo "Ignoring $name ($id) (modulo)\n";
+        continue;
+    }
+    # Start by cleaning our own data to make this script replayable
     pg_query("DELETE FROM city_closedway where relation_id=$id");
+    echo "Computing city closed ways for $name ($id) (progress: ".($loop_index-1)."/$count)\n";
+    time_start();
+    pg_query("INSERT INTO city_closedway SELECT $id, w.id, w.tags, cwg.geom from ways w,city_geom cg, closedway_geometry cwg   WHERE cwg.way_id = w.id AND ST_Intersects(cg.geom_dump, cwg.geom ) AND cg.relation_id=$id");
+    time_end("closed ways");
+  }
+
+  /* Third loop: city_nodes */
+  $result = pg_query($query);
+  $loop_index = 0;
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    $id = $line["id"];
+    $name = $line["name"];
+    if ($loop_index++ % $modulo != $selected_modulo) {
+        echo "Ignoring $name ($id) (modulo)\n";
+        continue;
+    }
+    # Start by cleaning our own data to make this script replayable
+    pg_query("DELETE FROM city_node where relation_id=$id");
+    echo "Computing city nodes for $name ($id) (progress: ".($loop_index-1)."/$count)\n";
+    time_start();
+    pg_query("INSERT INTO city_node SELECT $id,n.id, n.tags, n.geom from nodes n, city_geom cg WHERE ST_Intersects( cg.geom_dump, n.geom) AND cg.relation_id=$id AND array_length(akeys(n.tags), 1) > 0");
+    time_end("nodes");
+  }
+
+  /* Fourth loop: data */
+  $result = pg_query($query);
+  $loop_index = 0;
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    $id = $line["id"];
+    $name = $line["name"];
+    if ($loop_index++ % $modulo != $selected_modulo) {
+        echo "Ignoring $name ($id) (modulo)\n";
+        continue;
+    }
+    # Start by cleaning our own data to make this script replayable
     pg_query("DELETE FROM city_data where relation_id=$id");
     
-    // Compute ways in city
-    echo " Computing ways\n";time_start();
-    pg_query("INSERT INTO city_way SELECT $id,w.id, w.tags, wg.geom from ways w, city_geom cg, way_geometry wg WHERE wg.way_id = w.id AND ST_Intersects(cg.geom_dump, wg.geom) AND cg.relation_id=$id");
-    time_end("ways"); time_start();
-    echo " Computing nodes\n";
-    pg_query("INSERT INTO city_node SELECT $id,n.id, n.tags, n.geom from nodes n, city_geom cg WHERE ST_Intersects( cg.geom_dump, n.geom) AND cg.relation_id=$id AND array_length(akeys(n.tags), 1) > 0");
-    time_end("nodes"); time_start();
-    echo " Computing closed ways \n";
-    pg_query("INSERT INTO city_closedway SELECT $id, w.id, w.tags, cwg.geom from ways w,city_geom cg, closedway_geometry cwg   WHERE cwg.way_id = w.id AND ST_Intersects(cg.geom_dump, cwg.geom ) AND cg.relation_id=$id");
-    time_end("closedways");
-
-
-    echo " Computing stats\n";
+    echo "Computing stats for $name ($id) (progress: ".($loop_index-1)."/$count)\n";
     time_start("stats");
     /* Optional */
     $insee = get_one_data("SELECT tags -> 'ref:INSEE' as insee FROM relations where id=$id", "insee");
