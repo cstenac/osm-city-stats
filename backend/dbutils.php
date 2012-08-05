9b42393985c275769e7ba28013090e35ae635654
zorglub <zorglub@prx3.pole-aquinetic.fr>
2012-08-05 13:53:25 +0200

    Misc fixes:
     * Take db_search_path into account
     * Move city geom computation to more PHP (one query / city) to be able to
       ignore some cities that cause errors

--- backend/dbutils.php
+++ backend/dbutils.php
@@ -1,4 +1,10 @@
 <?php
+  function connect($db_conn_string, $db_search_path) {
+    $conn = pg_connect($db_conn_string) or die('Could not connect: ' . pg_last_error());
+    pg_query("SET search_path TO $db_search_path");
+    return $conn;
+  }
+
   function safe_dml_query($query) {
     echo "------------------------\n";
     echo "Executing PG query: $query\n";
@@ -11,4 +17,17 @@
     echo "Query time: $t ms\n";
     echo "------------------------\n";
   }
+  function dml_query($query) {
+    echo "------------------------\n";
+    echo "Executing PG query: $query\n";
+    $time_start = microtime(true);
+    $res = pg_query($query);
+    $time_end = microtime(true);
+    echo "Done executing $query";
+    $t = round(($time_end-$time_start)*1000);
+    echo "Query time: $t ms\n";
+    echo "------------------------\n";
+    return $res;
+  }
+
 ?>
