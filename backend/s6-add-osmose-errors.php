a9d3ee9c441fc6f161b46ca78225d4f4a76f4665
Clément Stenac <clement.stenac@exalead.com>
2012-08-19 14:17:29 +0200

    Osmose errors stuff

--- backend/s6-add-osmose-errors.php
+++ backend/s6-add-osmose-errors.php
@@ -43,21 +43,32 @@
   include("../config/config.php.inc");
 
   $conn = pg_connect($db_conn_string) or die('Could not connect: ' . pg_last_error());
+  if ($bbox) { 
+    $count = get_one_data("SELECT count(relation_id) as count from city_geom WHERE geom_dump &&  $bbox_query", "count");
+  } else {
+# Not yet incremental
+    $count = get_one_data("SELECT count(relation_id) as count from city_geom", "count");
+  }
+  echo "Obtained target admin count: $count\n";
+
+  if ($bbox) {
+    $query = "SELECT relation_id as id, city as name from city_geom WHERE geom_dump && $bbox_query";
+  } else {
+# Not yet incremental
+    $query = "SELECT relation_id as id, city as name from city_geom";
+  }
 
 
-  $query = "SELECT id, tags -> 'name' as name from relations WHERE tags -> 'admin_level' = '8'";
+
   $result = pg_query($query);
-
   $loop_index = 0;
+//  pg_query("begin");
   while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     $id = $line["id"];
     $name = $line["name"];
-
+    $loop_index++;
     
-    if ($loop_index++ % $modulo != $selected_modulo) {
-        echo "Ignoring $name ($id) (modulo)\n";
-        continue;
-    }
+    echo "Computing errors for $name ($id) (progress: ".($loop_index-1)."/$count)\n";
   
     // Compute numbers of l1, l2, l3 errors in city
     $l1 = get_one_data("SELECT COUNT(*) as count from osmose_event oe, city_geom cg WHERE ST_Within(oe.point, cg.geom_dump) AND oe.level = 1 AND cg.relation_id=$id", "count");
@@ -65,7 +76,7 @@
     $l3 = get_one_data("SELECT COUNT(*) as count from osmose_event oe, city_geom cg WHERE ST_Within(oe.point, cg.geom_dump) AND oe.level = 3 AND cg.relation_id=$id", "count");
 
     $finq =  "UPDATE city_data set l1_errors = $l1, l2_errors=$l2, l3_errors=$l3 where relation_id = $id";
-    echo "  $finq\n";
+    echo "   $finq\n";
     pg_query($finq);
   }
 ?>
